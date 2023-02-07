30697bf287efffbc64c88edd1c86af69321ba44f
Manith Luthria <mnl21@duke.edu>
2023-02-07 14:43:38 -0500

    fixed minor bug in Roster

--- Roster.py
+++ Roster.py
@@ -7,7 +7,7 @@ class Roster:
         which_team = 'homeTeam' if home else 'awayTeam'
 
         for player_dict in box[which_team]['players']:
-            players.append(Player(home, player_dict['personId'], board, box))
+            self.players.append(Player(home, player_dict['personId'], board, box))
 
     def update(self, board, box):
         for player in self.players:
